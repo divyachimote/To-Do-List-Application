5c7227981a2fb6ec924353de8573ce4ea6276320
divyachimote <divyachimote2016@gmail.com>
2025-06-24 01:28:27 +0530

    To do list

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+# todo/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete'),
+    path('complete/<int:task_id>/', views.complete_task, name='complete'),
+]
